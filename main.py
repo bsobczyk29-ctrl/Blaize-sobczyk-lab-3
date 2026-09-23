99809474d7a42fbeb117306250be70f164ce9254
Blaize Sobczyk <bsobczyk29@setoncatholic.org>
2026-09-23 18:20:20 +0000

    almost done

---
+++ main.py
@@ -0,0 +1,37 @@
+# Function that adds two numbers
+def add(x,y):
+    print(x+y)
+
+    
+
+x=int(input("Enter the first number"))
+y=int(input("Enter the second number"))
+
+add(x,y)
+
+# Function that adds two numbers
+def sub(x,y):
+    print(x-y)
+
+x=int(input("Enter the first number"))
+y=int(input("Enter the second number"))
+
+sub(x,y)
+
+# Function that adds two numbers
+def multi(x,y):
+    print(x*y)
+
+x=int(input("Enter the first number"))
+y=int(input("Enter the second number"))
+
+multi(x,y)
+
+# Function that adds two numbers
+def div(x,y):
+    print(x/y)
+
+x=int(input("Enter the first number"))
+y=int(input("Enter the second number"))
+
+div(x,y)
